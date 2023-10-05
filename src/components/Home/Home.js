070226274503d4b7b9db4af01e6a6619abd51cdf
Chitranshoo Prakash <chitranshoo.prakash@jarvis.consulting>
2023-10-05 22:24:27 +0530

    ignore unused import statements on prod build

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,8 +1,6 @@
 import classes from './Home.module.css';
 import Card from "../UI/Card/Card";
 import MovieList from "../MovieList/MovieList";
-import Movie from "../MovieList/Movie/Movie";
-import MovieForm from "../MovieList/MovieForm/MovieForm";
 const Home = (props) => {
     return (
         <div className={classes.homeContainer}>
